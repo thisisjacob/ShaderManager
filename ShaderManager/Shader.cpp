b6141bb09fef24d169f4b9e9691cb88d69b07129
thisisjacob <jacobholfelder@gmail.com>
2021-12-20 12:54:03 -0500

    Simplified role, changed name

--- ShaderManager/Shader.cpp
+++ ShaderManager/Shader.cpp
@@ -1,28 +1,32 @@
-#include "ShaderManager.h"
+#include "Shader.h"
 
-ShaderManager::ShaderManager() {
+Shader::Shader() {
 	vertexShaderId = -1;
 	fragmentShaderId = -1;
-	activeProgramId = -1;
+	programId = -1;
+	isBuilt = false;
 }
 
-unsigned int ShaderManager::GetUniformId(std::string uniformName) {
-	if (activeProgramId < 0) {
-		std::cerr << "ShaderManager.ModifyUniform Failed: No program active.\n";
+unsigned int Shader::GetUniformId(std::string uniformName) {
+	if (programId < 0) {
+		std::cerr << "Shader.ModifyUniform Failed: Program associated with Shader object not built.\n";
 		return -1;
 	}
-	auto uniformLocation = glGetUniformLocation(activeProgramId, uniformName.c_str());
+	auto uniformLocation = glGetUniformLocation(programId, uniformName.c_str());
 	if (uniformLocation < 0) {
-		std::cerr << "ShaderManager.ModifyUniform Failed: Invalid uniform provided. " << uniformName << "\n";
+		std::cerr << "Shader.ModifyUniform Failed: Invalid uniform provided. " << uniformName << "\n";
 		return -1;
 	}
 	return uniformLocation;
 }
 
-bool ShaderManager::AddShader(std::string filePath, unsigned int shaderType) {
+bool Shader::AddShader(std::string filePath, unsigned int shaderType) {
+	// No functionality for changing shaders after program is built, so disallow
+	if (isBuilt)
+		return false;
 	// If there is no matching shader enum, then this function fails
 	if (shaderType != GL_VERTEX_SHADER && shaderType != GL_FRAGMENT_SHADER) {
-		std::cerr << "ShaderManager.AddShader Failed: Invalid shaderType specified. Must be an OpenGL shader enum.\n";
+		std::cerr << "Shader.AddShader Failed: Invalid shaderType specified. Must be an OpenGL shader enum.\n";
 		return false;
 	}
 	// Read from the file
@@ -31,7 +35,7 @@ bool ShaderManager::AddShader(std::string filePath, unsigned int shaderType) {
 	reader.open(filePath);
 	// Check for reading failure
 	if (!reader) {
-		std::cerr << "ShaderManager.AddShader Failed: Error when reading shader from file " << filePath << "\n";
+		std::cerr << "Shader.AddShader Failed: Error when reading shader from file " << filePath << "\n";
 		return false;
 	}
 	buffer << reader.rdbuf();
@@ -50,28 +54,23 @@ bool ShaderManager::AddShader(std::string filePath, unsigned int shaderType) {
 	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
 	if (!success) {
 		glGetShaderInfoLog(shader, 512, NULL, infoLog);
-		std::cout << "ShaderManager.AddShader Failed: Compilation error. " << infoLog << "\n";
+		std::cout << "Shader.AddShader Failed: Compilation error. " << infoLog << "\n";
 		return false;
 	}
 	return true;
 }
 
-bool ShaderManager::BuildProgram(std::string newName) {
+bool Shader::BuildProgram(std::string newName) {
 	// Must at least have a vertex and fragment shader
 	if (vertexShaderId == -1 || fragmentShaderId == -1) {
-		std::cerr << "ShaderManager.BuildProgram Failed: No Vertex or Fragment shader compiled.\n";
-		return false;
-	}
-	// Program with given name already exists
-	if (programs.find(newName) != programs.end()) {
-		std::cerr << "ShaderManager.BuildProgram Failed: Provided newName of program already associated with another program.\n";
+		std::cerr << "Shader.BuildProgram Failed: No Vertex or Fragment shader compiled.\n";
 		return false;
 	}
 	unsigned int newProgram = glCreateProgram();
+	programId = newProgram;
 	glAttachShader(newProgram, vertexShaderId);
 	glAttachShader(newProgram, fragmentShaderId);
 	glLinkProgram(newProgram);
-	programs.insert({ newName, newProgram });
 	// TODO: Check link validity
 	// Should reset shader information if failed
 
@@ -81,32 +80,17 @@ bool ShaderManager::BuildProgram(std::string newName) {
 	fragmentShaderId = -1;
 }
 
-bool ShaderManager::UseProgram(std::string programName) {
-	auto program = programs.find(programName);
-	// Program not found
-	if (program == programs.end()) {
-		std::cerr << "ShaderManager.UseProgram Failed: No program of programName found.\n";
-		return false;
-	}
+bool Shader::UseProgram(std::string programName) {
 	// Enable program
-	glUseProgram(program->second);
-	activeProgramId = program->second;
+	glUseProgram(programId);
+	// TODO: Check for errors
 	return true;
 }
 
-unsigned int ShaderManager::GetProgram(std::string programName) {
-	auto program = programs.find(programName);
-	if (program == programs.end()) {
-		std::cerr << "ShaderManager.GetProgram Failed: No program of programName found.\n";
-		return false;
-	}
-	return program->second;
-}
-
-bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list<int> args) {
+bool Shader::ModifyUniform(std::string uniformName, std::initializer_list<int> args) {
 	int numArgs = args.size();
 	if (numArgs == 0 || numArgs > 4) {
-		std::cout << "ShaderManager.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
+		std::cout << "Shader.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
 		return false;
 	}
 	int uniformVal = GetUniformId(uniformName);
@@ -125,10 +109,10 @@ bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list
 	auto err = glGetError();
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list<float> args) {
+bool Shader::ModifyUniform(std::string uniformName, std::initializer_list<float> args) {
 	int numArgs = args.size();
 	if (numArgs == 0 || numArgs > 4) {
-		std::cout << "ShaderManager.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
+		std::cout << "Shader.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
 		return false;
 	}
 	int uniformVal = GetUniformId(uniformName);
@@ -147,10 +131,10 @@ bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list
 	auto err = glGetError();
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list<unsigned int> args) {
+bool Shader::ModifyUniform(std::string uniformName, std::initializer_list<unsigned int> args) {
 	int numArgs = args.size();
 	if (numArgs == 0 || numArgs > 4) {
-		std::cout << "ShaderManager.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
+		std::cout << "Shader.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
 		return false;
 	}
 	int uniformVal = GetUniformId(uniformName);
@@ -169,10 +153,10 @@ bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list
 	auto err = glGetError();
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list<double> args) {
+bool Shader::ModifyUniform(std::string uniformName, std::initializer_list<double> args) {
 	int numArgs = args.size();
 	if (numArgs == 0 || numArgs > 4) {
-		std::cout << "ShaderManager.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
+		std::cout << "Shader.ModifyUniform failed. Invalid number of arguments: " << numArgs << "\n";
 		return false;
 	}
 	int uniformVal = GetUniformId(uniformName);
@@ -191,38 +175,38 @@ bool ShaderManager::ModifyUniform(std::string uniformName, std::initializer_list
 	auto err = glGetError();
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, int val) {
+bool Shader::ModifyUniform(std::string uniformName, int val) {
 	unsigned int uniformVal = GetUniformId(uniformName);
 	if (uniformVal < 0) return false;
 	glUniform1i(uniformVal, val);
 	return true;
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, unsigned int val) {
+bool Shader::ModifyUniform(std::string uniformName, unsigned int val) {
 	unsigned int uniformVal = GetUniformId(uniformName);
 	if (uniformVal < 0) return false;
 	glUniform1ui(uniformVal, val);
 	return true;
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, float val) {
+bool Shader::ModifyUniform(std::string uniformName, float val) {
 	unsigned int uniformVal = GetUniformId(uniformName);
 	if (uniformVal < 0) return false;
 	glUniform1f(uniformVal, val);
 	return true;
 }
-bool ShaderManager::ModifyUniform(std::string uniformName, double val) {
+bool Shader::ModifyUniform(std::string uniformName, double val) {
 	unsigned int uniformVal = GetUniformId(uniformName);
 	if (uniformVal < 0) return false;
 	glUniform1d(uniformVal, val);
 	return true;
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, int rows, int cols, const glm::f32* matrix, bool trans) {
+bool Shader::ModifyUniform(std::string uniformName, int rows, int cols, const glm::f32* matrix, bool trans) {
 	return ModifyUniform(uniformName, rows, cols, matrix, 1, trans);
 }
 
-bool ShaderManager::ModifyUniform(std::string uniformName, int rows, int cols, const glm::f32* matrix, int numMatrices, bool trans) {
+bool Shader::ModifyUniform(std::string uniformName, int rows, int cols, const glm::f32* matrix, int numMatrices, bool trans) {
 	unsigned int uniformVal = GetUniformId(uniformName);
 	if (uniformVal < 0) return false;
 	// Determine which uniform function to use
@@ -246,7 +230,7 @@ bool ShaderManager::ModifyUniform(std::string uniformName, int rows, int cols, c
 	else if (rows == 4 && cols == 3)
 		glUniformMatrix4x3fv(uniformVal, numMatrices, transEnum, matrix);
 	else {
-		std::cerr << "ShaderManager::ModifyUniform(std::string, int, int, const glm::f32*) failed. Invalid number of rows and columns provided.";
+		std::cerr << "Shader::ModifyUniform(std::string, int, int, const glm::f32*) failed. Invalid number of rows and columns provided.";
 		return false;
 	}
 	// TODO: check errors with glGetError()
--- ShaderManager/Shader.h
+++ ShaderManager/Shader.h
@@ -6,19 +6,18 @@
 #include <iostream>
 #include <glm/glm.hpp>
 
-class ShaderManager {
+class Shader {
 private:
 	int vertexShaderId;
 	int fragmentShaderId;
-	int activeProgramId;
-	std::unordered_map<std::string, unsigned int> programs;
+	int programId;
+	bool isBuilt;
 	unsigned int GetUniformId(std::string uniformName);
 public:
-	ShaderManager();
+	Shader();
 	bool AddShader(std::string filePath, unsigned int shaderType);
 	bool BuildProgram(std::string newName);
 	bool UseProgram(std::string programName);
-	unsigned int GetProgram(std::string programName);
 	bool ModifyUniform(std::string uniformName, std::initializer_list<int> args);
 	bool ModifyUniform(std::string uniformName, std::initializer_list<unsigned int> args);
 	bool ModifyUniform(std::string uniformName, std::initializer_list<float> args);
